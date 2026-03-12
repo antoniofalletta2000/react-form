efedac7ae4391a64110921cbc6f90cdd65eada69
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-03-12 19:33:53 +0100

    exercise bonus

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ function App() {
   const articles = ["mele", "uova", "pasta", "pesto"]
   const [newTask, setNewTask] = useState("")
   const [tasks, setTasks] = useState(articles)
-  
+  const [edit, setEdit] = useState(-1)
+  const [editText, setEditText] = useState('')
+
   function handleSubmit(e) {
     e.preventDefault()
     if (newTask.length >= 3) {
@@ -23,7 +25,14 @@ function App() {
     setTasks(newArticles)
   }
 
- 
+  function saveArticle(index) {
+    const updatedTasks = [...tasks];
+    updatedTasks[index] = editText;
+    setTasks(updatedTasks);
+    setEdit(-1);
+  }
+
+
 
 
 
@@ -32,7 +41,7 @@ function App() {
       <form onSubmit={handleSubmit}>
         <div className="container">
           <div className='d-flex justify-content-center pt-3'>
-            <h1>ARTICOLI</h1>
+            <h1>ARTICOLI DA ACQUISTARE</h1>
           </div>
 
           <div className='d-flex justify-content-center pt-3 gap-3 pb-5'>
@@ -45,13 +54,28 @@ function App() {
       <div className="container">
         <ul className="list-group">
           {tasks.map((task, index) =>
-            <li className="bg-body-tertiary list-group-item d-flex justify-content-between align-items-center fw-bold fs-4" key={index}>
-              {task}
+            <li className="bg-body-tertiary list-group-item d-flex justify-content-between align-items-center fw-bold" key={index}>
 
+              <h2>{edit !== index ? task : <input className='border border-success rounded' value={editText} onChange={(e) => setEditText(e.target.value)} />}</h2>
               <div className='d-flex gap-1'>
-                <button className="btn text-bg-success rounded-pill"onClick={()=> edit(index) } >
-                  <i className="bi bi-pen"></i>
-                </button>
+                
+                {edit === index ? (
+                  <button
+                    className="btn btn-success rounded-pill"
+                    onClick={() => saveArticle(index)}
+                  >
+                    <i className="bi bi-check-lg"></i> 
+                  </button>
+                ) : (
+                  
+                  <button
+                    className="btn text-bg-primary rounded-pill"
+                    onClick={() => { setEditText(task); setEdit(index); }}
+                  >
+                    <i className="bi bi-pen"></i>
+                  </button>
+                )}
+
                 <button className="btn text-bg-danger rounded-pill" onClick={() => removeArticle(index)}>
                   <i className="bi bi-trash3"></i>
                 </button>
@@ -60,6 +84,9 @@ function App() {
 
             </li>
           )}
+          {tasks.length === 0 && <li className="list-group-item d-flex justify-content-center align-items-center border border-danger">
+            <h5>NON CI SONO ARTICOLI</h5>
+          </li>}
         </ul>
       </div>
 
