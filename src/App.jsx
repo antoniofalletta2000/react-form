3ae9d232fbf75c8cfeaed74ec92b3cabaa0e1963
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-03-12 14:24:13 +0100

    milestone 1 e milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,43 @@ import { useState } from 'react'
 
 
 function App() {
-  const articles=["mele", "uova", "pasta", "pesto"]
-  const [newTask, setNewTask]=useState("")
+  const articles = ["mele", "uova", "pasta", "pesto"]
+  const [newTask, setNewTask] = useState("")
+  const [tasks, setTasks]=useState(articles)
+
+  function handleSubmit(e){
+    e.preventDefault()
+    setTasks([newTask,...tasks])
+  }
 
   return (
     <>
+      <form onSubmit={handleSubmit}>
+        <div className="container">
+          <h1>ARTICOLI</h1>
+
+          <input type="text" placeholder='Type your articles' value={newTask} onChange={e => setNewTask(e.target.value)} />
+          <button>Invia</button>
+
+
+        </div>
+      </form>
       <div className="container">
-        <h1>ARTICOLI</h1>
-        <input type="text" placeholder='Type your articles' value={newTask} onChange={e=>setNewTask(e.target.value)}/>
-        <button>Invia</button>
+        <ul className="list-group">
+        {tasks.map((task,index)=>
+          <li className="list-group-item d-flex justify-content-between align-items-center" key={index}>
+            {task}
+            <span className="badge text-bg-primary rounded-pill">1</span>
+          </li>
+        )}
+        </ul>
       </div>
+      
+          
+        
+          
+      
+
     </>
   )
 }
